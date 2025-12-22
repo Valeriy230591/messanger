b8e057a4a2072fa3c2e345a70e1575d087b9e36f
Valeriy Boyko <valeriybojko42@gmail.com>
2025-12-22 16:08:45 +0300

    fix-toml

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,13 @@
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   server: {
+//     port: 3000,
+//   },
+//   preview: {
+//     port: 3000,
+//   },
+// });
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -6,5 +16,18 @@ export default defineConfig({
   },
   preview: {
     port: 3000,
+  },
+
+  base: "./",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]",
+      },
+    },
   },
 });
